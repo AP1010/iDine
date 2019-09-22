5d034a86445cc045ceaccad6c3ded1fc1fc624f2
Artur Pedyczak <arthur@caneris.ca>
2019-09-22 17:01:23 -0400

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Artur Pedyczak on 2019-09-22.
+//  Copyright © 2019 Artur Pedyczak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
